4c153b35269fe8d46cda6c4c746adf84bd5c3f82
Salonce <jakukaj@gmail.com>
2024-11-16 07:40:51 +0100

    fixed jackson dependency, made the bot run

--- src/main/java/dev/salonce/discordQuizBot/DiscordQuizBotApplication.java
+++ src/main/java/dev/salonce/discordQuizBot/DiscordQuizBotApplication.java
@@ -2,9 +2,14 @@ package dev.salonce.discordQuizBot;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import dev.salonce.discordQuizBot.Core.Answer;
+import dev.salonce.discordQuizBot.Core.Message;
 import dev.salonce.discordQuizBot.Core.Question;
+import dev.salonce.discordQuizBot.MessageHandlers.MessageHandlerChain;
 import discord4j.core.DiscordClient;
 import discord4j.core.GatewayDiscordClient;
+import discord4j.core.event.domain.message.MessageCreateEvent;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -14,11 +19,16 @@ import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
+@RequiredArgsConstructor
 @SpringBootApplication
 public class DiscordQuizBotApplication implements CommandLineRunner {
 
+	private final MessageHandlerChain messageHandlerChain;
+
 	@Value("${discord.bot.token}")
 	private String discordBotToken;
+
+
 
 	public static void main(String[] args) throws IOException {
 		SpringApplication.run(DiscordQuizBotApplication.class, args);
@@ -48,12 +58,9 @@ public class DiscordQuizBotApplication implements CommandLineRunner {
 		final GatewayDiscordClient gateway = client.login().block();
 
 
-//		gateway.on(MessageCreateEvent.class).subscribe(event -> {
-//			Message message = new Message();
-//			message.setMessage(event.getMessage());
-//
-//			messageHandlerChain.handle(message);
-//		});
+		gateway.on(MessageCreateEvent.class).subscribe(event -> {
+			messageHandlerChain.handle(new Message(event.getMessage()));
+		});
 		gateway.onDisconnect().block();
 	}
 }
