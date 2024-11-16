a489ccc41978225a1a302a58ebcabd219d77e90e
Salonce <jakukaj@gmail.com>
2024-11-16 09:38:38 +0100

    added questions

--- src/main/java/dev/salonce/discordQuizBot/DiscordQuizBotApplication.java
+++ src/main/java/dev/salonce/discordQuizBot/DiscordQuizBotApplication.java
@@ -57,7 +57,6 @@ public class DiscordQuizBotApplication implements CommandLineRunner {
 		final DiscordClient client = DiscordClient.create(discordBotToken);
 		final GatewayDiscordClient gateway = client.login().block();
 
-
 		gateway.on(MessageCreateEvent.class).subscribe(event -> {
 			messageHandlerChain.handle(new Message(event.getMessage()));
 		});
