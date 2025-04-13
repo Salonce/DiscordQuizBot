a4fce23308f7279b807d989f5bb0bf297b8754b3
salonce <miedzyniebem@gmail.com>
2025-04-13 19:30:36 +0200

    refactored: unansweredLimit from 2 to 3

--- src/main/java/dev/salonce/discordQuizBot/Configs/QuizConfigs.java
+++ src/main/java/dev/salonce/discordQuizBot/Configs/QuizConfigs.java
@@ -16,11 +16,11 @@ public class QuizConfigs {
         if (mode.equals("testing"))
             return new QuizConfig(5, 3, 7, 2, 5, 3);
         else if (mode.equals("testing2"))
-            return new QuizConfig(5, 2, 5, 5, 30, 3);
+            return new QuizConfig(5, 3, 5, 5, 30, 3);
         else if (mode.equals("standard"))
-            return new QuizConfig(10, 2, 30, 3, 30, 7);
+            return new QuizConfig(10, 3, 30, 3, 30, 7);
         else if (mode.equals("eureka"))
-            return new QuizConfig(10, 2, 15, 2, 15, 7);
+            return new QuizConfig(10, 3, 15, 2, 15, 7);
         else{
             System.out.println("ERROR! Mode " + mode + " doesn't exist! Check your application.yaml file for mode configuration.");
             return null;
