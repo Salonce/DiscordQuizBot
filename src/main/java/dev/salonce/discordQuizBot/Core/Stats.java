d72fd0b21a1378e51e8065a51831e0599f8d9d36
salonce <miedzyniebem@gmail.com>
2025-04-30 01:06:04 +0200

    stats part 1

---
+++ src/main/java/dev/salonce/discordQuizBot/Core/Stats.java
@@ -0,0 +1,8 @@
+package dev.salonce.discordQuizBot.Core;
+
+public class Stats {
+
+    //function that prints amount of guilds, their ids and names in a string on call
+
+    //function that increases amount of int matches whenever a match is called and prints it in console in form: Match nr X started by user Y in Guild of name Z
+}
