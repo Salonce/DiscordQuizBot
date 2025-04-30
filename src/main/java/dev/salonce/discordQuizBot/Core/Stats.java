fa014dd64a26b20d9d2c30feb6987ac0bfc9109e
salonce <miedzyniebem@gmail.com>
2025-04-30 15:57:59 +0200

    stats part 2

--- src/main/java/dev/salonce/discordQuizBot/Core/Stats.java
+++ src/main/java/dev/salonce/discordQuizBot/Core/Stats.java
@@ -1,8 +1,18 @@
 package dev.salonce.discordQuizBot.Core;
 
-public class Stats {
+import dev.salonce.discordQuizBot.Core.Matches.Match;
+import discord4j.core.object.entity.channel.MessageChannel;
 
+public class Stats {
+    private int matches_started = 0;
     //function that prints amount of guilds, their ids and names in a string on call
 
+    public void addMatch(Match match){
+        System.out.println("Starting match nr " + ++matches_started);
+        System.out.println("Creator: " + "<@" + match.getOwnerId() + ">");
+        System.out.println("Topic:" + match.getTopic() + " " + match.getDifficulty());
+        System.out.println();
+
+    }
     //function that increases amount of int matches whenever a match is called and prints it in console in form: Match nr X started by user Y in Guild of name Z
 }
