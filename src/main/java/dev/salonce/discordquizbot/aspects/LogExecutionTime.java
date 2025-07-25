c87f3ef0265da33e1367d1d65cc373d3efd7a7a7
Saloncey <miedzyniebem@gmail.com>
2025-07-25 22:32:25 +0200

    Added a logger for execution time of loading raw JSON questions from resources.

---
+++ src/main/java/dev/salonce/discordquizbot/aspects/LogExecutionTime.java
@@ -0,0 +1,29 @@
+package dev.salonce.discordquizbot.aspects;
+
+import dev.salonce.discordquizbot.core.questions.RawQuestion;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Aspect
+@Component
+public class LogExecutionTime {
+
+    @Pointcut("execution(public * dev.salonce.discordquizbot.core.questions.RawQuestionLoader.loadQuestionsFromResources(..))")
+    public void loadQuestionsFromResources(){};
+
+    @Around("loadQuestionsFromResources()")
+    public Object logLoadingQuestionsTime(ProceedingJoinPoint joinPoint) throws Throwable {
+        long startingTime = System.currentTimeMillis();
+        Object object = joinPoint.proceed();
+        long endTime = System.currentTimeMillis();
+        long executionTime = endTime - startingTime;
+        System.out.println("Loading raw JSON questions from resources took " + executionTime + " miliseconds.");
+        return object;
+    }
+}
