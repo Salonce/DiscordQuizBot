6c87922e36cfd2a8ee187ec8bfd329432b371aae
Saloncey <miedzyniebem@gmail.com>
2025-08-16 18:08:56 +0200

    removed bloat

--- src/main/java/dev/salonce/discordquizbot/domain/Player.java
+++ src/main/java/dev/salonce/discordquizbot/domain/Player.java
@@ -3,44 +3,31 @@ package dev.salonce.discordquizbot.domain;
 import lombok.Getter;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Player {
 
-    private final List<Integer> answersList;
-
     @Getter
     private int points;
 
-    public void setAnswer(int index, int answer){
-        this.answersList.set(index, answer);
-    };
+    private final List<Integer> answersList;
 
-    public void setAnswerAsLetter(int index, char letter) {
-        int answerNumber = letter - 'A';  // Convert 'A', 'B', 'C', etc. to 0, 1, 2...
-        answersList.set(index, answerNumber);
-    }
-
-    public String getAnswerAsLetter(int index) {
-        int answer = answersList.get(index);
-        return answer >= 0 ? String.valueOf((char) ('A' + answer)) : "?";
-    }
-
-    public boolean isUnanswered(int index){
-        return (answersList.get(index) == -1);
+    public Player(int numOfQuestions) {
+        this.points = 0;
+        this.answersList = new ArrayList<>(Collections.nCopies(numOfQuestions, -1));
     }
 
     public int getAnswer(int index){
         return answersList.get(index);
     }
 
-    public Player(int numOfAnswers){
-        this.points = 0;
-        answersList = new ArrayList<>();
-        for (int i = 0; i < numOfAnswers; i++){
-            answersList.add(-1);
-        }
-        //this.answersList = new ArrayList<>(Collections.nCopies(numOfAnswers, -1));
+    public void setAnswer(int index, int answer){
+        this.answersList.set(index, answer);
+    };
+
+    public boolean isUnanswered(int index){
+        return (answersList.get(index) == -1);
     }
 
     public void addPoint(){
