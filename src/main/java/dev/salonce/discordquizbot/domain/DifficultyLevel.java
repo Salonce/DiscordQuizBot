e99556b7a5fbcb6ddeed47c25fbee81c2682c017
Saloncey <miedzyniebem@gmail.com>
2025-08-17 04:00:48 +0200

    difficultylevel to record

--- src/main/java/dev/salonce/discordquizbot/domain/DifficultyLevel.java
+++ src/main/java/dev/salonce/discordquizbot/domain/DifficultyLevel.java
@@ -1,18 +1,6 @@
 package dev.salonce.discordquizbot.domain;
 
 import dev.salonce.discordquizbot.infrastructure.dtos.RawQuestion;
-import lombok.Getter;
-
-import java.util.ArrayList;
 import java.util.List;
 
-
-@Getter
-public class DifficultyLevel {
-
-    public DifficultyLevel(List<RawQuestion> rawQuestions){
-        this.rawQuestions = rawQuestions;
-    }
-
-    public List<RawQuestion> rawQuestions = new ArrayList<>();
-}
+public record DifficultyLevel(List<RawQuestion> rawQuestions) {}
