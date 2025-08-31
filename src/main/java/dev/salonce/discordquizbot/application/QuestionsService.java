9930227a4c5c3db1cbf9d5ea32782681687385bb
Saloncey <miedzyniebem@gmail.com>
2025-09-01 01:14:27 +0200

    improved and finished logging, i think

--- src/main/java/dev/salonce/discordquizbot/application/QuestionsService.java
+++ src/main/java/dev/salonce/discordquizbot/application/QuestionsService.java
@@ -6,10 +6,12 @@ import dev.salonce.discordquizbot.domain.Questions;
 import dev.salonce.discordquizbot.infrastructure.configs.QuizSetupConfig;
 import dev.salonce.discordquizbot.infrastructure.dtos.RawQuestion;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.util.*;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class QuestionsService {
@@ -61,7 +63,7 @@ public class QuestionsService {
         List<RawQuestion> rawQuestions = rawQuestionsService.getRawQuestionList(tag, difficulty);
         List<Question> questions = new ArrayList<>();
         if (rawQuestions.size() < NoQuestions)
-            System.out.println("Not enough questions in this category...");
+            log.debug("Not enough questions in category {}.", tag);
         for(int i = 0; i < NoQuestions; i++){
             int next = rand.nextInt(rawQuestions.size());
             questions.add(create(rawQuestions.get(next)));
@@ -77,7 +79,7 @@ public class QuestionsService {
         }
         List<Question> questions = new ArrayList<>();
         if (rawQuestions.size() < NoQuestions)
-            System.out.println("Not enough questions in this category...");
+            log.debug("Not enough questions in category {}.", tag);
         for(int i = 0; i < NoQuestions; i++){
             int next = rand.nextInt(rawQuestions.size());
             questions.add(create(rawQuestions.get(next)));
--- src/main/java/dev/salonce/discordquizbot/infrastructure/storage/RawQuestionStore.java
+++ src/main/java/dev/salonce/discordquizbot/infrastructure/storage/RawQuestionStore.java
@@ -3,10 +3,12 @@ package dev.salonce.discordquizbot.infrastructure.storage;
 import dev.salonce.discordquizbot.infrastructure.dtos.RawQuestion;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.util.*;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class RawQuestionStore {
@@ -28,8 +30,7 @@ public class RawQuestionStore {
         for (RawQuestion rawQuestion : this.rawQuestions){
             for (String tag : tags){
                 if (rawQuestion.tags() == null) {
-                    System.out.println("Missing or null tags for question ID: " + rawQuestion.id());
-                    System.out.println("question: " + rawQuestion.question());
+                    log.debug("Missing or null tags for question: ID: {}, question: {}", rawQuestion.id(), rawQuestion.question());
                     break;
                 }
                 if (rawQuestion.tags().contains(tag)) {
