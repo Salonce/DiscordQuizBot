db2e1574218ddb111b45a72d8631703c55ae07c3
Saloncey <miedzyniebem@gmail.com>
2025-09-01 23:41:42 +0200

    RawQuestionStore improvement

--- src/main/java/dev/salonce/discordquizbot/infrastructure/storage/RawQuestionStore.java
+++ src/main/java/dev/salonce/discordquizbot/infrastructure/storage/RawQuestionStore.java
@@ -5,23 +5,20 @@ import jakarta.annotation.PostConstruct;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.*;
 import java.util.stream.Collectors;
 
+@Getter
 @Slf4j
 @Component
-@RequiredArgsConstructor
 public class RawQuestionStore {
 
-    private final RawQuestionLoader rawQuestionLoader;
+    private final List<RawQuestion> rawQuestions;
 
-    @Getter
-    private List<RawQuestion> rawQuestions;
-
-    @PostConstruct
-    public void init(){
-        rawQuestions = rawQuestionLoader.loadQuestionsFromResources();
+    public RawQuestionStore(RawQuestionLoader rawQuestionLoader){
+        this.rawQuestions = rawQuestionLoader.loadQuestionsFromResources();
     }
 }
